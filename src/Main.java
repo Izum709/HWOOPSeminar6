2d7f76c2d36f476ce68ab1e87ecb8402e58aedfe
Alexandr Vinogradov <izum709@.com>
2023-12-22 18:45:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        User user = new User("Bob");
+        Persister persister = new Persister(user);
+        Report report = new Report();
+        report.report(user);
+        persister.save();
+    }
+}
